f1f8d53ae940e56f19384f6ced4027d9d998b3c6
YeHH <yhh1934292134@163.com>
2022-10-29 00:24:03 +0800

    add

--- src/main/java/org/example/homework12_12/Format.java
+++ src/main/java/org/example/homework12_12/Format.java
@@ -3,7 +3,12 @@ package org.example.homework12_12;
 import java.io.*;
 import java.util.*;
 
-public class format {
+/**
+ * @author Yehh
+ */
+public class Format {
+    static String lineSeparator = System.lineSeparator();
+
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
             System.out.println("format program require 2 arguments");
@@ -21,8 +26,24 @@ public class format {
         }
         try (Scanner input = new Scanner(sourceFile);
              PrintWriter output = new PrintWriter(targetFile);) {
+
         }
     }
 
-
+    void format(Scanner scanner, PrintWriter printWriter) {
+        while (scanner.hasNext()) {
+            String s1 = scanner.nextLine();
+            if (scanner.hasNext()) {
+                String s2 = scanner.nextLine();
+                if ("}".equals(s1)) {
+                    return;
+                }
+                if ("{".equals(s2)) {
+                    printWriter.println(s1.substring(0, s1.length() - Format.lineSeparator.length()) + " {");
+                    format(scanner, printWriter);
+                }
+            }
+        }
+        return;
+    }
 }
